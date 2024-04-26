80dfdd072e284760793d806599de589623974e57
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-04-26 16:05:55 -0500

    tests created

---
+++ A7Tests/FeaturesTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7.Tests
+{
+    [TestClass()]
+    public class FeaturesTests
+    {
+        [TestMethod()]
+        public void randomnumberTest()
+        {
+            var fun = new Features();
+            var r = fun.randomnumber();
+            Assert.IsTrue(r >= 1 && r <= 10);
+            Assert.IsNotNull(r);
+            Assert.IsFalse(r > 11);
+        }
+    }
+}
