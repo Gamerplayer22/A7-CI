71ad0374816484c6fcaa5cfe05d36d8f63f6f485
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-04-26 16:04:26 -0500

    adeed

---
+++ A7/Features.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7
+{
+    public class Features
+    {
+        // 1 ----------------------------------
+        public int randomnumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10) + 1;
+        }
+
+        // 2 ==================================
+        public string daytime()
+        {
+            DateTime thisDay = DateTime.Today;
+            return thisDay.ToShortDateString();
+        }
+
+        // 3 -=-==-=-=-==--=-==--=-=-=-=-=-=-=-=-=
+        public void unorderedlist(List<string> dinosaurs)
+        {
+            foreach (string d in dinosaurs)
+                Console.WriteLine(d);
+        }
+
+        public void orderedlist(List<string> dinosaurs)
+        {
+            var result = dinosaurs.OrderBy(n => n);
+            foreach (string d in dinosaurs)
+                Console.WriteLine(d);
+        }
+
+        public int randomdinosaur()
+        {
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+            return (rnd1.Next(6) + rnd2.Next(5));
+        }
+
+        // 4 =-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        public string stringrandom(string str)
+        {
+            Random rnd = new Random();
+
+            switch (rnd.Next(10))
+            {
+                case 0:
+                    str = str.Reverse().ToString();
+                    return str;
+                case 1:
+                    str = str.Substring(1, 5);
+                    return str;
+                case 2:
+                    str = str.ToUpper();
+                    return str;
+                case 3:
+                    str = str.ToLower();
+                    return str;
+                case 4:
+                    str = str.GetHashCode().ToString();
+                    return str;
+                case 5:
+                    str = str.Replace('a', 'e');
+                    return str;
+                case 6:
+                    str = str.Remove(2, 6);
+                    return str;
+                case 7:
+                    str = str.Split().ToString();
+                    return str;
+            }
+            return str;
+        }
+    }
+}
--- A7/Program.cs
+++ A7/Program.cs
@@ -10,6 +10,81 @@ namespace A7
     {
         static void Main(string[] args)
         {
+            int choice = 1;
+            string x = "5";
+            string str;
+            var funcs = new Features();
+
+            List<string> dinosaurs = new List<string>();
+            dinosaurs.Add("Gorgosaurus");
+            dinosaurs.Add("Allosaurus");
+            dinosaurs.Add("Aardonyx");
+            dinosaurs.Add("Megalosaurus");
+            dinosaurs.Add("Abrosaurus");
+            dinosaurs.Add("Homalocephale");
+            dinosaurs.Add("Alvarezsaurus");
+            dinosaurs.Add("Datousaurus");
+            dinosaurs.Add("Ammosaurus");
+            dinosaurs.Add("Capitalsaurus");
+
+            while (choice == 1)
+            {
+                Console.WriteLine("Main menu");
+                Console.WriteLine("1. random number");
+                Console.WriteLine("2. today`s date");
+                Console.WriteLine("3. list of dinosaurs");
+                Console.WriteLine("4. string magic");
+                Console.WriteLine("5. exit");
+                Console.WriteLine("Choose 1-5:");
+                x = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (x == "1")
+                {
+                    int r = funcs.randomnumber();
+                    Console.WriteLine("The random number: {0}", r);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                if (x == "2")
+                {
+                    string s = funcs.daytime();
+                    Console.WriteLine("Today's date: {0}", s);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                if (x == "3")
+                {
+                    Console.WriteLine("unordered list: ");
+                    funcs.unorderedlist(dinosaurs);
+                    Console.WriteLine();
+                    Console.WriteLine("ordered list: ");
+                    funcs.orderedlist(dinosaurs);
+                    Console.WriteLine();
+                    var r = funcs.randomdinosaur();
+                    Console.WriteLine("Random dinosaur picked: {0}", dinosaurs[r]);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                if (x == "4")
+                {
+                    Console.WriteLine("give a string, at least 10 characters:");
+                    str = Console.ReadLine();
+                    Console.WriteLine("something random happens to the string:");
+                    string s = funcs.stringrandom(str);
+                    Console.WriteLine(s);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                if (x == "5") { choice = 0; }
+            }
+
+            Console.WriteLine("Exiting program... Press enter");
+            Console.ReadLine();
         }
     }
 }
